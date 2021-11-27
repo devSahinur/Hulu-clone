9417291b55961722e517bde9ba74f01a272f1f26
Sahinur Islam <infosahinur@gmail.com>
2021-11-28 01:28:12 +0600

    nav added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       {/* Header */}
        <Header />
       {/* Nav */}
-      
+       <Nav />
       {/* Results */}
     </div>
   )
