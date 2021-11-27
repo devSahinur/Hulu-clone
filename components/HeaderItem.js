5670781bb5fc842adef140953e2038e2624c9e2e
Sahinur Islam <infosahinur@gmail.com>
2021-11-27 23:47:05 +0600

    header done

---
+++ components/HeaderItem.js
@@ -0,0 +1,11 @@
+
+function HeaderItem({ Icon, title }) {
+    return (
+        <div className='group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white'>
+            <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+            <p className=" tracking-widest opacity-0 group-hover:opacity-100">{title}</p>
+        </div>
+    )
+}
+
+export default HeaderItem
